88726a28472340c5863ad05546d4d2c65e71cbfc
Ibrahim35789 <147152916+Ibrahim35789@users.noreply.github.com>
2023-10-06 22:45:12 +0500

    Add files via upload

---
+++ my calculator.cs
@@ -0,0 +1,50 @@
+﻿// My Calculator in C# (Ibrahim 35789)
+
+
+using System;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        
+           public static double Result;
+           public static void Add(double A, double B)
+    {
+            Result = A + B;
+        }
+
+           public static void Subtract(double A, double B)
+    {
+            Result = A - B;
+        }
+
+           public static void Multiply(double A, double B)
+    {
+            Result = A * B;
+        }
+
+           public static void Divide(double A, double B)
+    {
+            Result = A / B;
+        }
+
+           public static void Main(string[] args)
+    {
+            Add(2.12458, 30);
+            Console.WriteLine("Answer is: " + Result);
+
+            Multiply(6, 9.5);
+            Console.WriteLine("Answer is: " + Result);
+
+            Subtract(150, 100);
+            Console.WriteLine("Answer is: " + Result);
+
+            Divide(90,5.9);
+            Console.WriteLine("Answer is: " + Result);
+            
+
+        }
+    }
+}
+
