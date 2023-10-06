af3db47db6e195909d2961ca8f825e3ad26759cd
Ibrahim35789 <147152916+Ibrahim35789@users.noreply.github.com>
2023-10-06 22:50:42 +0500

    Add files via upload

---
+++ variables code.cs
@@ -0,0 +1,146 @@
+//Code using variables (Kia haal hai?)
+using System;
+
+namespace Variables 
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string name1 = "Lilly";
+            int age1 = 21;
+            float average1 = 2.9F;
+            bool pass1 = true;
+            char grade1 = 'B';
+            
+            Console.WriteLine("Name: " + name1);
+            Console.WriteLine("Age: " + age1);
+            Console.WriteLine("Average: " + average1);
+            Console.WriteLine("Pass: " + true);
+            Console.WriteLine("Grade: " + 'B');
+            Console.WriteLine();
+
+            string name2 = "Anna williams";
+            int age2 = 23;
+            float average2 = 3.8F;
+            bool pass2 = true;
+            char grade2 = 'A';
+            
+            Console.WriteLine("Name: " + name2);
+            Console.WriteLine("Age: " + age2);
+            Console.WriteLine("Average: " + average2);
+            Console.WriteLine("Pass: " + true);
+            Console.WriteLine("Grade: " + 'A');
+            Console.WriteLine();
+
+            string name3 = "Nina williams";
+            int age3 = 22;
+            float average3 = 3.4F;
+            bool pass3 = true;
+            char grade3 = 'A';
+            
+            Console.WriteLine("Name: " + name3);
+            Console.WriteLine("Age: " + age3);
+            Console.WriteLine("Average: " + average3);
+            Console.WriteLine("Pass: " + true);
+            Console.WriteLine("Grade: " + 'A');
+            Console.WriteLine();
+
+            string name4 = "Akuma";
+            int age4 = 40;
+            float average4 = 3.9F;
+            bool pass4 = true;
+            char grade4 = 'A';
+            
+            Console.WriteLine("Name: " + name4);
+            Console.WriteLine("Age: " + age4);
+            Console.WriteLine("Average: " + average4);
+            Console.WriteLine("Pass: " + true);
+            Console.WriteLine("Grade: " + 'A');
+            Console.WriteLine(); 
+
+            string name5 = "Eddy";
+            int age5 = 27;
+            float average5 = 3.2F;
+            bool pass5 = true;
+            char grade5 = 'B';
+            
+            Console.WriteLine("Name: " + name5);
+            Console.WriteLine("Age: " + age5);
+            Console.WriteLine("Average: " + average5);
+            Console.WriteLine("Pass: " + true);
+            Console.WriteLine("Grade: " + 'B');
+            Console.WriteLine();
+
+            string name6 = "Lars";
+            int age6 = 20;
+            float average6 = 1.2F;
+            bool pass6 = false;
+            char grade6 = 'F';
+            
+            Console.WriteLine("Name: " + name6);
+            Console.WriteLine("Age: " + age6);
+            Console.WriteLine("Average: " + average6);
+            Console.WriteLine("Pass: " + false);
+            Console.WriteLine("Grade: " + 'F');
+            Console.WriteLine();
+
+            string name7 = "Negan";
+            int age7 = 40;
+            float average7 = 3.1F;
+            bool pass7 = true;
+            char grade7 = 'A';
+            
+            Console.WriteLine("Name: " + name7);
+            Console.WriteLine("Age: " + age7);
+            Console.WriteLine("Average: " + average7);
+            Console.WriteLine("Pass: " + true);
+            Console.WriteLine("Grade: " + 'A');
+            Console.WriteLine();
+
+            string name8 = "Kazuya";
+            int age8 = 30;
+            float average8 = 1.9F;
+            bool pass8 = false;
+            char grade8 = 'C';
+            
+            Console.WriteLine("Name: " + name8);
+            Console.WriteLine("Age: " + age8);
+            Console.WriteLine("Average: " + average8);
+            Console.WriteLine("Pass: " + false);
+            Console.WriteLine("Grade: " + 'C');
+            Console.WriteLine();
+
+            string name9 = "Kazumi";
+            int age9 = 25;
+            float average9 = 3.0F;
+            bool pass9 = true;
+            char grade9 = 'B';
+            
+            Console.WriteLine("Name: " + name9);
+            Console.WriteLine("Age: " + age9);
+            Console.WriteLine("Average: " + average9);
+            Console.WriteLine("Pass: " + true);
+            Console.WriteLine("Grade: " + 'B');
+            Console.WriteLine();
+
+            string name10 = "Heihachi";
+            int age10 = 55;
+            float average10 = 3.5F;
+            bool pass10 = false;
+            char grade10 = 'A';
+            
+             
+            Console.WriteLine("Name: " + name10);
+            Console.WriteLine("Age: " + age10);
+            Console.WriteLine("Average: " + average10);
+            Console.WriteLine("Pass: " + false);
+            Console.WriteLine("Grade: " + 'A');
+            Console.WriteLine();
+
+
+            // Hello Sir 
+            
+        }
+    }
+}
